2da910922d8b47186d5d25b3fbfcf1c97cd3a344
Pam Willenz <pam@positek.net>
2015-08-19 11:30:35 -0400

    Fixed delete on topics#index, posted bookmarks on topics#show using rails c

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
   get 'about' => 'welcome#about'
 
   root to: 'welcome#index'
-    post :incoming, to: 'incoming#create'
+  post :incoming, to: 'incoming#create'
 end
