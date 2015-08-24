3be4635e3bf49c24777b9c7e4482b6ddf7d88bfa
Pam Willenz <pam@positek.net>
2015-08-24 15:57:28 -0400

    Finally, able to create, update and delete bookmarks on topics#show page

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -6,7 +6,7 @@ class TopicsController < ApplicationController
   def show
     @topic = Topic.find(params[:id])
     @bookmarks = @topic.bookmarks
-    @bookmark = @bookmarks.new
+    @bookmark = Bookmark.new
   end
 
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   
+  devise_for :users
+  
   resources :topics do
     resources :bookmarks, except: [:index]
   end
 
-  devise_for :users
+  
   
   get 'about' => 'welcome#about'
 
