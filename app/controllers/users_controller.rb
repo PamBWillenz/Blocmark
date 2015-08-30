988ed97918d911c498b409375d5cd96850f4aaf8
Pam Willenz <pam@positek.net>
2015-08-30 09:46:16 -0400

    User Profile page now shows bookmarks and liked bookmarks, able to delete likes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,25 @@
 class UsersController < ApplicationController
-  before_action: :authenticate_user!, except: [:show]
+  before_action :authenticate_user!, except: [:show]
 
   def show
     @user = current_user
     @user_bookmarks = @user.bookmarks
-    @liked_bookmarks = @user.likes
+    @liked_bookmarks = @user.liked_bookmarks
+  end
+
+  def update
+    if current_user.update_attributes(user_params)
+      flash[:notice] = "User information updated."
+      redirect_to edit_user_registration_path
+    else
+      flash[:error] = "Invalid user information"
+      redirect_to edit_user_registration_path
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
   end
 end
