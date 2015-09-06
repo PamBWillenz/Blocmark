38d89d8fd5271e202f9fdb31af5a60d1b5bcef56
Pam Willenz <pam@positek.net>
2015-09-06 11:26:16 -0400

    still working on getting topics edit and delete to show

--- app/policies/topic_policy.rb
+++ app/policies/topic_policy.rb
@@ -1,18 +1,18 @@
 class TopicPolicy < ApplicationPolicy
 
-  def index?
-    true
-  end
+  # def index?
+  #   true
+  # end
 
-  def update?
-    user.present? && (record.user == user)
-  end
+  # def update?
+  #   user.present? && (record.user == user)
+  # end
 
-  def destroy?
-    update?
-  end
+  # def destroy?
+  #   update?
+  # end
 
-  def edit?
-    update?
-  end
+  # def edit?
+  #   update?
+  # end
 end
