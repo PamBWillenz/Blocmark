93102ab99c1222c98d3227daaf873e2eff31281b
Pam Willenz <pam@positek.net>
2015-09-09 12:03:42 -0400

    change topic#index, #show, user#show views with embedly

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,7 @@ require 'json'
 module ApplicationHelper
 
   def display(url)
-    embedly_api = Embedly::API.new :key => ENV['EMBEDLY_API_KEY'], :user_agent => 'Mozilla/5.0 (compatible; blocmark/1.0; pam@positek.net)'
+    embedly_api = Embedly::API.new :key => ENV['EMBEDLY_API_KEY'], :user_agent => 'Mozilla/5.0 (compatible; pam-blocmarks/1.0; pam-blocmarks@app5988cc52158c402ab5689904615024dc.mailgun.org)'
     obj = embedly_api.oembed :url => url
     #obj.first.html_safe
     # (obj.first.thumbnail_url).html_safe
